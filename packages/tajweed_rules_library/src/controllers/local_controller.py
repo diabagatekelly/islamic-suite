8de6fec0dbbf88668840c39c011ca1f6d5def0e0
Kelly Diabagate <diabagatekelly@yahoo.fr>
2022-01-30 22:12:42 -0500

    Add docstrings for factory, local controller, prod controller

--- packages/tajweed_rules_library/src/controllers/local_controller.py
+++ packages/tajweed_rules_library/src/controllers/local_controller.py
@@ -1,5 +1,4 @@
 import os
-
 from src.factory import Factory
 from src.use_cases.create_rules_maps import CreateRulesMaps
 from src.use_cases.choose_rule_maps_to_create import ChooseRuleMapsToCreate
@@ -10,23 +9,58 @@ ENTITIES_DIR = os.path.join(ROOT, 'entities')
 OUTPUTS_DIR = os.path.join(ROOT, 'outputs')
 
 FILES_SYS = {
-	'root': ROOT,
-	'input_file': INPUT_FILE,
-	'entities_dir': ENTITIES_DIR,
-	'outputs_dir': OUTPUTS_DIR
+  'root': ROOT,
+  'input_file': INPUT_FILE,
+  'entities_dir': ENTITIES_DIR,
+  'outputs_dir': OUTPUTS_DIR
 }
-    
+
 class LocalController():
+  """Local (Dev) Controller
+  
+  Instantiates CreateRulesMaps class (the package's primary use case)
+  in a dev environment, which means all outputed JSON maps are saved to
+  the local file system.
+
+  Parameters:
+    *env - defaults to local
+    *files - defaults to local file directories
+      *root: the root directory 
+      *input_file: the directoy where the Uthmani Quran text file
+      used to create each rule's map is saved
+      *entities_dir: the directory where the classes for mapping each tajweed rule
+      are saved
+      *outputs_dir: the directory where outputed JSON maps are saved 
+  
+  Constructor:
+    *files parameter
+    *Factory: instantiated with the env parameter 
+
+  Functions:
+    *choose_rules_to_map (private) - choose rules to map
+    *create_rule_maps - create JSON maps or chosen rules
+  """
   def __init__(self, env='local', files=FILES_SYS):
     self
     self.factory = Factory(env)
     self.files = files
 
   def create_rule_maps(self):
+    """Create JSON maps for chosen rules.
+    Instantiates CreateRulesMaps with local factory and files.
+    """
     rule_creator = CreateRulesMaps(self.factory, files_and_dirs=self.files)
     rule_selector = self._choose_rules_to_map()
     rule_creator.create_rule_maps(rule_selector)
 
   def _choose_rules_to_map(self):
-    rule_selector = ChooseRuleMapsToCreate(factory=self.factory, files_and_dirs=self.files)
+    """Choose the tajweed rules that need to be mapped,
+    based on whether an JSON map doesn't already exist,
+    or one exists but there have been recent changes to the rule's
+    mapping class.
+    Instantiates ChooseRuleMapsToCreate with local factory and files.
+    """
+    rule_selector = ChooseRuleMapsToCreate(
+      factory=self.factory, files_and_dirs=self.files
+    )
     return rule_selector.get_list_of_rule_maps_to_create()
--- packages/tajweed_rules_library/src/factory.py
+++ packages/tajweed_rules_library/src/factory.py
@@ -1,18 +1,34 @@
 from src.gateways.file_system import FileSystem
 from src.gateways.file_to_map_gateway import FileToMapGateway
 
-# In the future I might add a S3 file handler
 class Factory():
+  """Factory for selecting:
+    *Local file system or online file system gateway (perhaps in the future)
+    *File system to JSON map translator gateway
+  
+  Constructor:
+    *env - defaults to local
+ 
+  Returns:
+    *FileSystem class
+    *FileToMapGateway class
+
+  Functions:
+    *get_file_system - returns FileSystem class
+    *get_file_to_map_gateway - returns FileToMapGateway class
+  """
   def __init__(self, env='local'):
     self
     self.env = env
     
   def get_file_system(self):
+    """Returns FileSystem class or online file system."""
     if self.env in ['local', 'prod']:
       return FileSystem()
     elif self.env == 'online':
       return 'online file Urls'
    
   def get_file_to_map_gateway(self):
-      return FileToMapGateway()
+    """Returns FileToMapGateway class."""
+    return FileToMapGateway()
 
