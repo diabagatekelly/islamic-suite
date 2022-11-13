41872bd0e10a5ac7dc76eb90a20e3e535c83869d
Kelly Diabagate <diabagatekelly@yahoo.fr>
2022-11-13 16:34:44 -0500

    Updated controllers + specs

--- packages/tajweed_rules_library/src/controllers/prod_controller.py
+++ packages/tajweed_rules_library/src/controllers/prod_controller.py
@@ -8,14 +8,14 @@ LOCAL_OUTPUTS = os.path.join(ROOT, 'outputs')
 PROD_OUTPUTS = os.path.join(ROOT, 'dist')
 
 FILES_SYS = {
-	'root': ROOT,
-	'local_outputs': LOCAL_OUTPUTS,
-	'outputs_dir': PROD_OUTPUTS
+  'root': ROOT,
+  'local_outputs': LOCAL_OUTPUTS,
+  'outputs_dir': PROD_OUTPUTS
 }
-    
+
 class ProdController():
   """Prod (Production) Controller
-  
+
   Instantiates CreateRulesMaps class (the package's primary use case)
   in a prod environment, which means all outputed JSON maps are saved to
   a S3 bucket by CI/CD pipeline.
@@ -23,16 +23,16 @@ class ProdController():
   Parameters:
     *env - defaults to prod
     *files - builds a dist output directory from a local outputs directory
-      *root: the root directory 
-      *local_outputs: the local outputs directory
-      *outputs_dir: the dist outpus directory
-  
+    *root: the root directory 
+    *local_outputs: the local outputs directory
+    *outputs_dir: the dist outpus directory
+
   Constructor:
     *files parameter
     *Factory: instantiated with the prod parameter 
 
   Functions:
-    *choose_rules_to_map (private) - choose rules to map
+    *_get_local_maps_to_copy (private) - get the details of the local JSON maps that need to be copied
     *create_rule_maps - create JSON maps or chosen rules
   """
   def __init__(self, env='prod', files=FILES_SYS):
@@ -42,17 +42,16 @@ class ProdController():
 
   def create_rule_maps(self):
     """Create JSON maps for chosen rules.
-      Instantiates CreateRulesMaps with local factory and files.
+      Instantiates CreateRulesMaps with prod factory and files.
     """
     rule_creator = CreateRulesMaps(self.factory, files_and_dirs=self.files)
-    rule_selector = self._choose_rules_to_map()
-    rule_creator.create_rule_maps(rule_selector)
+    rules_to_copy = self._get_local_maps_to_copy()
+    rule_creator.create_rule_maps(rules_to_copy)
 
-  def _choose_rules_to_map(self):
-    """Choose the JSON maps that need to be copied to the dist folder,
-      based on whether they don't already exist, or if there have been
-      recent changes to the local outputes JSON maps.
+  def _get_local_maps_to_copy(self):
+    """Get the details of the local JSON maps that need to be copied 
+      to the dist folder.
       Instantiates ChooseRuleMapsToCreate with prod factory and files.
     """
-    rule_selector = ChooseRuleMapsToCreate(factory=self.factory, files_and_dirs=self.files)
-    return rule_selector.get_list_of_rule_maps_to_create()
+    rules_to_copy = ChooseRuleMapsToCreate(factory=self.factory, files_and_dirs=self.files)
+    return rules_to_copy.get_list_of_json_maps_to_create()
