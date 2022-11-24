d7879fc4986882714c5ebf35338e0245a579f216
Kelly Diabagate <diabagatekelly@yahoo.fr>
2022-11-24 18:57:22 -0500

    Update mock fixtures, file system + specs

--- packages/tajweed_rules_library/src/gateways/file_system.py
+++ packages/tajweed_rules_library/src/gateways/file_system.py
@@ -1,134 +1,138 @@
-import os, json, shutil
+import os
+import json
+import shutil
 
 class FileSystem():
-	"""File System Gateway
-  
+  """File System Gateway
+
   Gateway to the local file system which allows getting files from
-	lists of directories, getting files in nested directories, getting
-	the last update of, reading from, and writing to files, create file
-	paths, and copying files to other directories. 
+  lists of directories, getting files in nested directories, getting
+  the last update of, reading from, and writing to files, create file
+  paths, and copying files to other directories.
 
   Functions:
-    *get_files_in_directory - get the files in a given directory
-			- parameters: directory
-			- returns: array of file names
     *_get_nested_directories (private) - get the directories nested within a given directory
-			- parameters: directory
-			- returns: array of objects with a file's name and absolute path
-		*_get_factory_path_for_file (private) - get the absolute path for the rule factory associated with a particular rule
-			- parameters: path to directory, filename for given rule
-			- returns: absolute path of factory file
-		*get_file_last_update_date - get last update date for a file
-			- parameters: file path
-			- returns: float
-		*read_file_by_lines - read file line by by line
-			- parameters: file path
-			- returns: array of strings, where each string is a file line
-		*create_absolute_path - concatenates the root path, a directory path, and a rule name to create 
-			an absolute path for a new file json file
-			- parameters: directory path, rule
-			- returns: JSON file absolute path as string
-		*write_to_file - save content to a file
-			- parameters : file path, content
-		*copy_file_from_original_to_target_dir - copy file to a new directory
-			- parameters: original file path, destination file path
+    
+    *get_files_in_directory - get the files in a given directory
+    *read_file_by_lines - read file line by by line
+    *create_absolute_path - concatenates the root path, a directory path, and a rule name to create
+    an absolute path for a new file json file 
+    *write_json_to_file - save json content to a file
+    *write_text_to_file - save list content to a text file
+    *copy_file_from_original_to_target_dir - copy file to a new directory
+    *empty_directory - delete all the content inside a directory
+    *delete_directory - delete a directory and its content
   """
-	def __init__(self):
-		self
 
-	def get_files_in_directory(self, directory):
-		"""Get the files in a given directory
-			- parameters: directory path
-			- returns: array of file names
+  def __init__(self):
+    self
+
+  def get_files_in_directory(self, directory):
+    """Get the files in a given directory
+      - parameters: directory absolute path
+      - returns: array of file objects
+      [{
+        'name': filename.py,
+        'absolute_path': 'C://path/to/file.py'
+      }]
     """
-		filenames = []
-		subdir_paths = self._get_nested_directories(directory)
+    filenames = []
+    subdir_paths = self._get_nested_directories(directory)
 
-		if len(subdir_paths) != 0:
-			# This block iterates over nested directories insise the entities directory.
-			# These are our definition files
-			for path in subdir_paths:
-				for subdir, dirs, files in os.walk(path):
-					for filename in files:
-						# Do not return info for __init__.py, tests, or factory files
-						if filename.endswith('py') and not any(map(filename.__contains__, ['init', 'factory', 'test'])):
-							file_stats = {
-								'name': filename,
-								'absolute_path': os.path.join(path, filename),
-								'factory_path': self._get_factory_path_for_file(path, filename)
-							}
-							filenames.append(file_stats)
-		else:
-			# This block iterates over the flat output directories (outputs or dist).
-			# These are our exisiting output JSON files.
-			for subdir, dirs, files in os.walk(directory):
-				for filename in files:
-					file_stats = {
-						'name': filename,
-						'absolute_path': os.path.join(directory, filename)
-					}
-					filenames.append(file_stats)
-		return filenames
+    if len(subdir_paths) != 0:
+      # This block iterates over nested directories insise the entities directory.
+      # These define how to create the maps for each tajweed rule (each entity).
+      for path in subdir_paths:
+        for subdir, dirs, files in os.walk(path):
+          for filename in files:
+            # Do not return info for __init__.py, tests, or factory files
+            if filename.endswith('py') and not any(map(filename.__contains__, ['test', '__init__'])):
+              file_stats = {
+                'name': filename,
+                'absolute_path': os.path.join(path, filename)
+              }
+              filenames.append(file_stats)
+    else:
+      # This block iterates over the flat output directories ('outputs' in local env or 'dist' in prod env).
+      # These are our exisiting output JSON files.
+      for subdir, dirs, files in os.walk(directory):
+        for filename in files:
+          file_stats = {
+            'name': filename,
+            'absolute_path': os.path.join(directory, filename)
+          }
+          filenames.append(file_stats)
+    return filenames
 
-	def _get_nested_directories(self, directory):
-		"""Get the directories nested within a given directory
-			- parameters: directory path
-			- returns: array of objects with a file's name and absolute path
-		"""
-		subdirectories_paths = []
-		with os.scandir(directory) as subdirectory:
-			for entry in subdirectory:
-				if not entry.is_file() and entry.name != '__pycache__':
-					subdirectories_paths.append(entry.path)
-		return subdirectories_paths
+  def _get_nested_directories(self, directory):
+    """Get the directories nested within a given directory
+      - parameters: directory absolute path
+      - returns: array of file objects
+      [{
+        'name': filename.py,
+        'absolute_path': 'C://path/to/file.py'
+      }]
+    """
+    subdirectories_paths = []
+    with os.scandir(directory) as subdirectory:
+      for entry in subdirectory:
+        if not entry.is_file() and entry.name != '__pycache__':
+          subdirectories_paths.append(entry.path)
+    return subdirectories_paths
 
-	def _get_factory_path_for_file(self, path, filename):
-		"""Get the absolute path for the rule factory associated with a particular rule
-			- parameters: path to directory, filename for given rule
-			- returns: absolute path of factory file
-		"""
-		if any(map(filename.__contains__, ['idghaam_shafawi', 'idhaar_shafawi', 'ikhfa_shafawi'])):
-			return os.path.join(path, 'meem_saakin_rule_factory.py')
+  def read_file_by_lines(self, file):
+    """Read file line by by line
+      - parameters: file path
+      - returns: array of strings, where each string is a file line
+    """
+    file_content = []
+    file = open(file, 'r', encoding='utf-8')
+    for line in file.readlines():
+      file_content.append(line)
+    file.close()
+    return file_content
 
-	def get_file_last_update_date(self, file_path):
-		"""Get last update date for a file
-			- parameters: file path
-			- returns: date as float
-		"""
-		return os.stat(file_path).st_mtime
+  def create_absolute_path(self, path, rule):
+    """Concatenates the root path, a directory path, and a rule name
+      to create an absolute path for a new JSON file
+      - parameters: directory path, rule name
+      - returns: JSON file absolute path as string
+    """
+    relative_path = os.path.join(path["outputs_dir"], f'{rule}.json')
+    absolute_path = os.path.join(path["root"], relative_path)
+    return absolute_path
 
-	def read_file_by_lines(self, file):
-		"""Read file line by by line
-			- parameters: file path
-			- returns: array of strings, where each string is a file line
-		"""
-		file_content = []
-		file = open(file, 'r', encoding='utf-8')
-		for line in file.readlines():
-			file_content.append(line)
-		file.close()
-		return file_content
+  def write_json_to_file(self, content, path):
+    """Write JSON to a file
+      - parameters : JSON content, file path
+    """
+    with open(path, "w") as outfile:
+      json.dump(content, outfile)
+      outfile.close()
+          
+  def write_text_to_file(self, list_content, path):
+    """Write text inside a list to a text file
+      - parameters : list of strings, file path
+    """
+    with open(path, 'w') as f:
+      f.write('\n'.join(list_content))
+      f.close()
+          
+  def copy_file_from_original_to_target_dir(self, original, target):
+    """Copy file to a new directory
+      - parameters: original file path, destination file path
+    """
+    shutil.copyfile(original, target)
 
-	def create_absolute_path(self, path, rule):
-		"""Concatenates the root path, a directory path, and a rule name
-			to create an absolute path for a new JSON file
-			- parameters: directory path, rule name
-			- returns: JSON file absolute path as string
-		"""
-		relative_path = os.path.join(path["outputs_dir"], f'{rule}.json')
-		absolute_path = os.path.join(path["root"], relative_path)
-		return absolute_path
+  def empty_directory(self, directory):
+    """Delete all the files in a given directory
+      - parameters: directory
+    """
+    for file in os.listdir(directory):
+      os.remove(os.path.join(directory, file))
 
-	def write_to_file(self, content, path):
-		"""Write to a file
-			- parameters : file path, content
-		"""
-		with open(path, "w") as outfile:
-			json.dump(content, outfile)
-			outfile.close()
-
-	def copy_file_from_original_to_target_dir(self, original, target):
-		"""Copy file to a new directory
-			- parameters: original file path, destination file path
-		"""
-		shutil.copyfile(original, target)
+  def delete_directory(self, directory):
+    """Delete a directory and its content
+      - parameters: directory
+    """
+    shutil.rmtree(directory)
