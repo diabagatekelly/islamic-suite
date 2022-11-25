93693cb839cefe1eeaccc8ad6cb1a982d67a489f
Kelly Diabagate <diabagatekelly@yahoo.fr>
2022-11-24 22:56:07 -0500

    comment out unnecessary rules

--- packages/tajweed_rules_library/src/tajweed_rules_helpers/tajweed_rules_map.py
+++ packages/tajweed_rules_library/src/tajweed_rules_helpers/tajweed_rules_map.py
@@ -7,17 +7,17 @@
 # from src.entities.misc_letter_rules.qalqalah import Qalqalah
 
 # Noon Saakin Rules
-from src.entities.noon_saakin_rules.noon_saakin_rule_factory import NoonSaakinRuleFactory
-from src.entities.noon_saakin_rules.idghaam_ghunnah import IdghaamGhunnah
-from src.entities.noon_saakin_rules.idghaam_no_ghunnah import IdghaamNoGhunnah
-from src.entities.noon_saakin_rules.idhaar import Idhaar
-from src.entities.noon_saakin_rules.ikhfa import Ikhfa
-from src.entities.noon_saakin_rules.iqlab import Iqlab
+# from src.entities.noon_saakin_rules.noon_saakin_rule_factory import NoonSaakinRuleFactory
+# from src.entities.noon_saakin_rules.idghaam_ghunnah import IdghaamGhunnah
+# from src.entities.noon_saakin_rules.idghaam_no_ghunnah import IdghaamNoGhunnah
+# from src.entities.noon_saakin_rules.idhaar import Idhaar
+# from src.entities.noon_saakin_rules.ikhfa import Ikhfa
+# from src.entities.noon_saakin_rules.iqlab import Iqlab
 
 # Meem Saakin Rules
 from src.entities.meem_saakin_rules.meem_saakin_rules import MeemSaakinRules
 
 TAJWEED_RULES = {
   'MeemSaakinRules': MeemSaakinRules,
-  'NoonSaakinRuleFactory': NoonSaakinRuleFactory
+  # 'NoonSaakinRuleFactory': NoonSaakinRuleFactory
 }
